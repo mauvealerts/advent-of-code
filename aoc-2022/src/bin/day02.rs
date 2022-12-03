66da6d014003db1ecf6cda416375353f5a6889f3
Mauve <mauve@google.com>
2022-12-03 11:41:47 -0500

    Day 2 part 2

--- aoc-2022/src/bin/day02.rs
+++ aoc-2022/src/bin/day02.rs
@@ -1,5 +1,11 @@
 use anyhow::{Result, anyhow};
 
+#[derive(Debug, PartialEq, Eq)]
+struct Answer {
+    part1: u32,
+    part2: u32,
+}
+
 fn main() -> Result<()> {
     let d = include_str!("../../data/challenge/day02.txt");
     println!("{:#?}", solve(d)?);
@@ -10,6 +16,12 @@ enum Choice {
     Rock,
     Paper,
     Scissors,
+}
+
+enum Outcome {
+    Lose,
+    Draw,
+    Win,
 }
 
 impl Choice {
@@ -28,6 +40,20 @@ impl Choice {
             'Y' => Ok(Choice::Paper),
             'Z' => Ok(Choice::Scissors),
             _ => Err(anyhow!("Unrecognized {}", c))
+        }
+    }
+
+    fn for_outcome(&self, outcome: Outcome) -> Self {
+        match (self, outcome) {
+            (Choice::Rock, Outcome::Lose) => Choice::Scissors,
+            (Choice::Rock, Outcome::Draw) => Choice::Rock,
+            (Choice::Rock, Outcome::Win) => Choice::Paper,
+            (Choice::Paper, Outcome::Lose) => Choice::Rock,
+            (Choice::Paper, Outcome::Draw) => Choice::Paper,
+            (Choice::Paper, Outcome::Win) => Choice::Scissors,
+            (Choice::Scissors, Outcome::Lose) => Choice::Paper,
+            (Choice::Scissors, Outcome::Draw) => Choice::Scissors,
+            (Choice::Scissors, Outcome::Win) => Choice::Rock,
         }
     }
 
@@ -54,18 +80,32 @@ impl Choice {
     }
 }
 
-fn solve(input: &str) -> Result<u32> {
-    let mut total = 0_u32;
+impl Outcome {
+    fn from_char(c: char) -> Result<Self> {
+        match c {
+            'X' => Ok(Outcome::Lose),
+            'Y' => Ok(Outcome::Draw),
+            'Z' => Ok(Outcome::Win),
+            _ => Err(anyhow!("Unrecognized {}", c))
+        }
+    }
+}
+
+fn solve(input: &str) -> Result<Answer> {
+    let mut part1 = 0;
+    let mut part2 = 0;
     for (i,l) in input.lines().enumerate() {
         let l: Vec<_> = l.chars().collect(); 
         if l.len() < 3 {
             return Err(anyhow!("line {} length is {}, expected 3", i, l.len()));
         }
         let other = Choice::from_opponent(l[0])?;
-        let mine = Choice::from_self(l[2])?;
-        total += mine.shape_score() + mine.match_score(&other);
+        let mine_part1 = Choice::from_self(l[2])?;
+        let mine_part2 = other.for_outcome(Outcome::from_char(l[2])?);
+        part1 += mine_part1.shape_score() + mine_part1.match_score(&other);
+        part2 += mine_part2.shape_score() + mine_part2.match_score(&other);
     }
-    Ok(total)
+    Ok(Answer { part1, part2 })
 }
 
 #[cfg(test)]
@@ -75,6 +115,6 @@ mod tests {
     #[test]
     fn example() {
         let answer = solve(include_str!("../../data/example/day02.txt")).unwrap();
-        assert_eq!(answer, 15);
+        assert_eq!(answer, Answer { part1: 15, part2: 12});
     }
 }
